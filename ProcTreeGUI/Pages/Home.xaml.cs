d07e5bdab61b41da72a707a553fcbc2a4c43a7b6
ManGregory <manaenkov.gregory@gmail.com>
2014-09-28 14:42:48 +0300

    initiated the development of an graphical interface

---
+++ ProcTreeGUI/Pages/Home.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ProcTree.Core;
+
+namespace ProcTreeGUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void LstDbObjects_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TxtDbObjectSource.Text = (LstDbObjects.SelectedItem as DbObject).Source;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dbRepo = new DbObjectRepository(
+                TxtUserName.Text, TxtUserPassword.Password, TxtServerName.Text, TxtDbName.Text
+            );
+            var dbObjects =
+                dbRepo.GetDbObjects()
+                    .Select(
+                        d =>
+                            new DbObject
+                            {
+                                Name = d.Name.ToUpperInvariant(),
+                                Source = d.Source,
+                                Type = d.Type,
+                                LinkedDbOjbects = d.LinkedDbOjbects
+                            })
+                    .Where(d => d.Type == DbObjectType.Procedure)
+                    .OrderBy(d => d.Name)
+                    .ToList();
+            LstDbObjects.ItemsSource = dbObjects;
+            LstDbObjects.SelectedIndex = dbObjects.Count > 0 ? 0 : -1;
+        }
+    }
+}
